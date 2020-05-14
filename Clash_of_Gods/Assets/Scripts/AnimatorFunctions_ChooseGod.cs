e2238837d7f0839a396cfd7674fbdd2a9085f0b1
ThePigeon109 <patryklochowo@interia.pl>
2020-05-14 21:31:04 +0200

    ORLIK: Create animations in secondary menu

---
+++ Clash_of_Gods/Assets/Scripts/AnimatorFunctions_ChooseGod.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorFunctions_ChooseGod : MonoBehaviour
+{
+    [SerializeField] Choose_God_ButtonController chooseGodButtonController;
+    public bool disableOnce;
+
+    void PlaySound(AudioClip whichSound)
+    {
+        if (!disableOnce)
+        {
+            chooseGodButtonController.audioSource.PlayOneShot(whichSound);
+        }
+        else
+        {
+            disableOnce = false;
+        }
+    }
+}
---
+++ Clash_of_Gods/Assets/Scripts/Choose_God_Button.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Choose_God_Button : MonoBehaviour
+{
+    [SerializeField] Choose_God_ButtonController chooseGodButtonController;
+    [SerializeField] Animator animator;
+    [SerializeField] AnimatorFunctions_ChooseGod animatorFunctionsChooseGod;
+    [SerializeField] int thisIndexHorizontal,thisIndexVertical;
+
+    void Update()
+    {
+        if (chooseGodButtonController.indexHorizontal == thisIndexHorizontal && chooseGodButtonController.indexVertical == thisIndexVertical)
+        {
+            animator.SetBool("selected", true);
+            if (Input.GetAxis("Submit") == 1)
+            {
+                animator.SetBool("pressed", true);
+            }
+            else if (animator.GetBool("pressed"))
+            {
+                animator.SetBool("pressed", false);
+                animatorFunctionsChooseGod.disableOnce = true;
+            }
+        }
+        else
+        {
+            animator.SetBool("selected", false);
+        }
+    }
+}
