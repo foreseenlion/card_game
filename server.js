66308a41a3c8e77692fa6c0a00557db54674a627
DESKTOP-724CT6A\Lukasz-laptop <miklas.lukasz11@gmail.com>
2020-11-02 21:54:04 +0100

    Add start functions

--- server.js
+++ server.js
@@ -2,18 +2,115 @@
 var server = require('http').createServer();
 var io = require('socket.io')(server);
 
+var watiForGame = null;
+var games = {};
+var playersGames = {}
+
 // This defines the port that we'll be listening to
 server.listen(3000);
 
-io.sockets.on('connection', function(socket)
-{
-	console.log('User connected: ' + socket.id);
-	socket.emit('connectionEstabilished', {id: socket.id});
-	socket.broadcast.emit('connectionEstabilished', {id: socket.id});
+io.sockets.on('connection', function (socket) {
 
-	socket.on('move', function(data)
-	{
-		console.log(data.message);
-		socket.broadcast.emit("foreignMessage", {id: socket.id, message: data.message});
+	console.log('User connected: ' + socket.id);
+	// Mechanizm tworzenia i przydzielania gier
+	checkGame(socket.id)
+
+	socket.emit('connectionEstabilished', { id: socket.id });
+	socket.broadcast.emit('connectionEstabilished', { id: socket.id });
+
+	socket.on('move', function (data) {
+		move(data, socket)
 	});
+
+	socket.on('end', function (data) {
+		tureEnd(data, socket)
+	});
+
+	socket.on('playerMove', function (data) {
+		playerMove(data, socket)
+	});
+
 });
+
+function tureEnd(data, socket) {
+	console.log("hi")
+	var gameTmp = games[playersGames[socket.id]]
+	var idPlayer = replyPlayer(gameTmp, socket.id)
+	socket.emit("tureEnd", { id: socket.id, ture: "0" });
+	socket.to(socket.id).emit("tureEnd", { id: socket.id, ture: "0" });
+}
+
+function move(data, socket) {
+	console.log(data.PoleY)
+	console.log(data.PoleX)
+	var gameTmp = games[playersGames[socket.id]]
+	var idPlayer = replyPlayer(gameTmp, socket.id)
+	// idPlayer powinno byc argumentem funkcji to  (nie jest bo debaguje)
+	socket.emit("foreignMessage", { id: socket.id, x: data.PoleX, y: data.PoleY });
+	socket.to(socket.id).emit("foreignMessage", { id: socket.id, y: data.PoleY ,x: data.PoleX,});
+}
+
+
+function playerMove(data, socket) {
+	var gameTmp = games[playersGames[socket.id]]
+	var idPlayer = replyPlayer(gameTmp, socket.id)
+	// idPlayer powinno byc argumentem funkcji to 
+	socket.emit("moveTo", {
+		id: socket.id,
+		idPionka: data.idPionka,
+		poleStartoweX: data.poleStartoweX,
+		poleStartoweY: data.poleStartoweY,
+		poleDoceloweX: data.poleDoceloweX,
+		poleDoceloweY: data.poleDoceloweY,
+	});
+	socket.to(socket.id).emit("moveTo", {
+		id: socket.id,
+		idPionka: data.idPionka,
+		poleStartoweX: data.poleStartoweX,
+		poleStartoweY: data.poleStartoweY,
+		poleDoceloweX: data.poleDoceloweX,
+		poleDoceloweY: data.poleDoceloweY,
+	});
+}
+
+
+
+function replyPlayer(gameTmp, idPlayer) {
+	if (gameTmp.userWhite != idPlayer) {
+		return gameTmp.userWhite;
+	} else {
+		return gameTmp.userBlack;
+	}
+}
+
+
+function checkGame(idPlayer) {
+	// Zmiena waitForGame zawiera id gry do ktorej moze dolaczyc jeszcze jeden gracz
+	if (watiForGame == null) {
+		// jezeli zmiena jest pusta tworzona jest nowa gra
+		var id = idGenerate()
+		games[id] = new game()
+		games[id].gameId = id;
+		games[id].userWhite = idPlayer
+		watiForGame = id
+		playersGames[idPlayer] = id
+	}
+	else {
+		games[watiForGame].userBlack = idPlayer
+		playersGames[idPlayer] = watiForGame
+		watiForGame = null
+	}
+}
+
+
+
+function idGenerate() {
+	return '_' + Math.random().toString(36).substr(2, 9);
+};
+
+
+class game {
+	gameId;
+	userWhite;
+	userBlack;
+}
