c83831dab66b5f9355869c09f96e2eed980f9d01
Patryk <patryklochowo@interia.pl>
2021-01-12 17:08:28 +0100

    Minor fixes

---
+++ Clash_of_Gods/Assets/Scripts/StatisticsMenu/BackButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackButton : MonoBehaviour
+{
+	[SerializeField] Animator buttonAnimator;
+	[SerializeField] AudioSource audioSource;
+	//[SerializeField] AudioClip click;
+	[SerializeField] AudioClip enter;
+
+	public void select()
+	{
+		buttonAnimator.SetBool("selected", true);
+		audioSource.PlayOneShot(enter);
+	}
+
+	public void deselect()
+	{
+
+		buttonAnimator.SetBool("selected", false);
+	}
+
+	public void press()
+	{
+		buttonAnimator.SetBool("pressed", true);
+		//audioSource.PlayOneShot(click);
+	}
+}
